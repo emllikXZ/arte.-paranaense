3ef0e77e15c2ebf4a8fdfe8c36b7a2f467c89c8d
kerollynstadler-collab <kerollyn.stadler@escola.pr.gov.br>
2026-09-15 11:12:58 -0300

    Alterei o script

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const botoesCurtir = document.querySelectorAkk(".curtir");
 
+botoesCurtir.forEach(funtion(botaoCurtir){
+    let curtiu = false;
+    botaocurtir.addEventListener("click", curtir);
+
+function curtir(){
+    const contador = botaoCurtir.querySelector("span");
+    if(curtiu === false){ 
+    contador.rexrContent++;
+    curtiu = true;
+    else{
+        con.textContent--;
+        curtiu = false;
+    }
+}
+}
+});
